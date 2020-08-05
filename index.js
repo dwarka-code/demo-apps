03758cd61dca14c6ff49d724569e82b8d05790c4
dwarka-code <tiwaridwarka@gmail.com>
2020-08-05 20:48:53 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+import './bootstrap.min.css';
 
 class App extends Component {
   constructor() {
